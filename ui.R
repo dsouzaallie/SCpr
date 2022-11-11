331b46345cbca74e1dd6ebd3f30188a2f59642a4
Alexandria D'Souza <alexandriadsouza@gmail.com>
2022-11-11 14:29:07 -0500

    Add files via upload
    App Update.

--- ui.R
+++ ui.R
@@ -5,13 +5,8 @@ shinyUI(fluidPage(theme=shinytheme("darkly"),
                   tabsetPanel(type="tabs",
                               #Output Plots onto actual Tabs.
                               tabPanel("SCREEN.",
-                                       fileInput('input$filename$datapath', 'Single Cell Filename'),
-                                       actionButton('runPCA', 'PCA.'),
-                                       actionButton('normMatrix', 'Normalization.'),
-                                       actionButton('createHeat', 'Heatmap.'),
-                                       actionButton('createFrequency', 'Frequency.'),
-                                       actionButton('createVenn', 'Venn.'),
-                                       actionButton('createCorrelation', 'Correlation.')),
+                                       fileInput('ImputedFile', 'Single Cell Filename'),
+                                       actionButton('runSCREEN', 'SCREEN')),
                          
                   )
 )
